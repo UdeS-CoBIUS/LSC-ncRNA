b3cd744415b7ba1b21f30c0f4a516efd710369ef
ibrahim chegrane <ibra.chegrane@gmail.com>
2022-11-24 19:31:18 -0500

    one family count

---
+++ supplementary/experiment_dataset_length.py
@@ -0,0 +1,11 @@
+# experiment_dataset_length:
+#  generate a graph where on the X axis put family size (number of seqs),
+#  and on Y axis the average len of seqs.
+#  this for 3 dataset:
+#  1) Rfam dataset,
+#  2) Noise dataset for 0 noise,
+#  3) Clan dataset (and for Rfam from clans)
+#
+# 	  for that we have compute for each family 1) number of seqs, and the average len seqs.
+
+print("hello ibra")
