196a9c3be6ca9cf45195e6a1530e2b8ccdfe9fde
ibrahim chegrane <ibra.chegrane@gmail.com>
2023-03-05 21:37:40 -0500

    fix errors

--- supplementary/merge_files_same_names.py
+++ supplementary/merge_files_same_names.py
@@ -6,9 +6,14 @@ this code is with help of chatGPT generator
 """
 
 # set directory paths
-input_dir1 = "/path/to/dir1"
-input_dir2 = "/path/to/dir2"
-output_dir3 = "/path/to/dir3"
+dir_base = "/data/chei2402/ibra/test_infernal/Rfam_14.1_dataset/Rfam14.1_Sample_Train_Test/Rfam_500_Train_Test/"
+input_dir1 = dir_base + "Train/"
+input_dir2 = dir_base + "Test/"
+output_dir3 = dir_base + "Train_plus_Test/"
+
+print ("dir 1 : " + input_dir1)
+print ("dir 2 : " + input_dir2)
+print ("dir 3 : " + output_dir3)
 
 # get list of files in dir1 and dir2
 files1 = os.listdir(input_dir1)
@@ -16,10 +21,10 @@ files2 = os.listdir(input_dir2)
 
 # Iterate through the list of file names and merge the files with the same name
 file_nb = 0
-for file_name in input_dir1:
-    file_nb = +1
+for file_name in files1:
+    file_nb = file_nb + 1
     print(" {} : {}".format(file_nb, file_name))
-    if file_name in input_dir2:  # if file with the same name is in dir2
+    if file_name in files2:  # if file with the same name is in dir2
         file1_path = os.path.join(input_dir1, file_name)
         file2_path = os.path.join(input_dir2, file_name)
         file3_path = os.path.join(output_dir3, file_name)
@@ -27,4 +32,4 @@ for file_name in input_dir1:
             file3.write(file1.read())
             file3.write(file2.read())
 
-print "finished :) "
+print ("finished :)")
