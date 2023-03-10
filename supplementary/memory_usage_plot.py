4acdcf7e0a35272b5853fdc930c915859782b5c2
ibrahim chegrane <ibra.chegrane@gmail.com>
2023-03-10 16:16:53 -0500

    add get_max_memory_usage

--- supplementary/memory_usage_plot.py
+++ supplementary/memory_usage_plot.py
@@ -19,3 +19,21 @@ plt.xlabel('Time')
 plt.ylabel('Memory Usage (GB)')
 plt.xticks(rotation=45)
 plt.show()
+
+
+
+def get_max_memory_usage():
+    # Read in memory usage data from log file
+    with open('memory.log', 'r') as f:
+        lines = f.readlines()
+
+    # Extract memory usage values and track maximum value
+    max_memory_usage = 0
+    for line in lines:
+        if 'Memory usage' in line:
+            memory_usage = float(line.split(': ')[-1].strip().replace('GB', ''))
+            if memory_usage > max_memory_usage:
+                max_memory_usage = memory_usage
+
+    # Print maximum memory usage
+    print(f"Maximum memory usage: {max_memory_usage:.2f} GB")
