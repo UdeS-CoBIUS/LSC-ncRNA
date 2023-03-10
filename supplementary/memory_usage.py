56ed1cb168e3f478195b6b878d4a03e73dac5533
ibrahim chegrane <ibra.chegrane@gmail.com>
2023-03-10 16:12:34 -0500

    add time

--- supplementary/memory_usage.py
+++ supplementary/memory_usage.py
@@ -9,8 +9,10 @@ logging.basicConfig(filename='memory.log', level=logging.INFO)
 def log_memory_usage():
     # Get memory usage in GB
     memory_usage = psutil.virtual_memory().used / 1024 / 1024 / 1024
-    # Log memory usage to file
-    logging.info(f"Memory usage: {memory_usage:.2f} GB")
+    # Get current time
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    # Log memory usage and timestamp to file
+    logging.info(f"{current_time} Memory usage: {memory_usage:.2f} GB")
 
 # Schedule task to run every 2.5 minutes
 while True:
